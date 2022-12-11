702efa2063a5d5fcd158f49ad1b2292c05847382
yurika0705 <116805970+yurika0705@users.noreply.github.com>
2022-12-11 11:34:45 +0900

    デプロイの為の設定

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module FoodTruck
     config.load_defaults 6.0
 
     config.i18n.default_locale = :ja
+    config.hosts << 'xxx.onrender.com'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
