39225d98d66e34a1196c15fc4b2e53c7de28af91
yurika0705 <116805970+yurika0705@users.noreply.github.com>
2022-12-22 12:16:33 +0900

    マイページの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,19 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show]
   def show
+    @user = current_user
+    @events = Event.where(user_id: @user.id)
   end
 
   private
+
+  def event_params
+		params.require(:event).permit(:title).merge(location_id: params[:location_id])
+	end
+
+  def location_params
+    params.require(:location).permit(:plase, :address, :contact_address, :fee, :info, :image, :prefecture_id, :latitude, :longitude)
+  end
 
   def set_user
     @user = User.find(params[:id])
