f6dd3c6b77dfbeaeae6b7ac60446109b02920f3c
yurika0705 <116805970+yurika0705@users.noreply.github.com>
2022-12-22 23:19:56 +0900

    トップページのレイアウト修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,16 +6,8 @@ class UsersController < ApplicationController
   end
 
   private
-
-  def event_params
-		params.require(:event).permit(:title).merge(location_id: params[:location_id])
-	end
-
-  def location_params
-    params.require(:location).permit(:plase, :address, :contact_address, :fee, :info, :image, :prefecture_id, :latitude, :longitude)
-  end
-
+  
   def set_user
-    @user = User.find(params[:id])
+    @user = current_user
   end
 end
