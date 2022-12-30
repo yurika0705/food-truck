a2ec0adc8c32b6666fadf381eeb9d1eafafb8390
yurika0705 <116805970+yurika0705@users.noreply.github.com>
2022-12-30 12:19:02 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show]
   before_action :correct_user
   def show
     @events = Event.where(user_id: current_user.id)
@@ -8,7 +7,7 @@ class UsersController < ApplicationController
 
   def correct_user
       @user = User.find(params[:id])
-    unless @user.user.id == current_user.id
+    unless @user.id == current_user.id
       redirect_to root_path
     end
   end
@@ -16,9 +15,5 @@ class UsersController < ApplicationController
 
   def user_events_params
     params.require(:location).permit(:place, :address, :contact_address, :fee, :info, :image, :prefecture_id, :latitude, :longitude).merge(user_id: current_user.id)
-  end
-
-  def set_user
-    @user = current_user
   end
 end
